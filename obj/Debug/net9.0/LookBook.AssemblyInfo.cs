47aa70c777e9317a9dd61a9a2f7134fd943f320d
Micheal Sokoya <151823022+MileyDev@users.noreply.github.com>
2025-05-14 22:43:42 +0000

    commit

--- obj/Debug/net9.0/LookBook.AssemblyInfo.cs
+++ obj/Debug/net9.0/LookBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LookBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6fad2d69af7b4bd3ed926fa27633bda1e6d2739")]
 [assembly: System.Reflection.AssemblyProductAttribute("LookBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LookBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
